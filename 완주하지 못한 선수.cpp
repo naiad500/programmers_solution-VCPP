10c3b772749f0536294fa8e7597b7181c94dd735
naiad500 <95596755+naiad500@users.noreply.github.com>
2021-12-06 11:08:59 +0900

    Update 완주하지 못한 선수.cpp

--- 완주하지 못한 선수.cpp
+++ 완주하지 못한 선수.cpp
@@ -7,16 +7,18 @@ using namespace std;
 string solution(vector<string> participant, vector<string> completion) {
     string answer = "";
     int ii;
-    
+    /* 정렬 */
     sort(participant.begin(), participant.end());
     sort(completion.begin(), completion.end());
-    
+
+    /* 비교 */
     for (ii = 0; ii < completion.size(); ii++)
     {
         if (participant[ii] != completion[ii])
             break;
     }
-    
+
+    /* 결과 */
     answer = participant[ii];
     
     return answer;
